2e1caaa9b7dcdd7a63725949eaa1ea724755c5f4
Stéphano Telolahy <stephano.telolahy@gmail.com>
2014-12-08 21:54:37 +0000

    icon

--- app/src/main/java/com/telolahy/solitaire/MainActivity.java
+++ app/src/main/java/com/telolahy/solitaire/MainActivity.java
@@ -19,45 +19,5 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView txtRawResource = (TextView) findViewById(R.id.txtRawResource);
-        txtRawResource.setText(readRawTextFile(this, R.raw.rawtextsample));
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    public static String readRawTextFile(Context ctx, int resId) {
-        InputStream inputStream = ctx.getResources().openRawResource(resId);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        String line;
-        StringBuilder text = new StringBuilder();
-        try {
-            while ((line = reader.readLine()) != null) {
-                text.append(line);
-                text.append('\n');
-            }
-        } catch (IOException e) {
-            return null;
-        }
-        return text.toString();
-    }
-
 }
